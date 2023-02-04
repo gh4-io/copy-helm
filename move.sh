4c0c680b443ed1675f5e0e68d9bdd1fe934ade1b
Jason <chrispnugget88@yahoo.com>
2023-02-03 22:25:02 -0500

    commit

--- move.sh
+++ move.sh
@@ -10,7 +10,7 @@ CLONE_DIR=$(mktemp -d)
 echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
-git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://$INPUT_USER_NAME:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+git clone --single-branch --branch "$INPUT_DESTINATION_BRANCH" "https://$INPUT_USER_NAME:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 if [ -n "$INPUT_DESTINATION_BRANCH_CREATE" ]
 then
@@ -20,7 +20,7 @@ fi
 
 echo "Copying contents to git repo"
 # shellcheck disable=SC2115
-if [ -z $INPUT_DESTINATION_FOLDER ]
+if [ -z "$INPUT_DESTINATION_FOLDER" ]
 then
   rm -rf "$CLONE_DIR/"*
   cp -r "$INPUT_SOURCE_FOLDER/"* "$CLONE_DIR/"
@@ -30,4 +30,4 @@ else
   cp -a "$INPUT_SOURCE_FOLDER/." "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
 fi
 
-cd "$CLONE_DIR"
+cd "$CLONE_DIR" || exit
